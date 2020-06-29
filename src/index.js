a1b5571043932f7aefc747c0d2585862d77a865c
Orlando Groppo <orlando.groppo@gmail.com>
2020-06-29 23:17:02 +0100

    Added GA

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-171207101-1");
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
