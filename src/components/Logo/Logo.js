49f1b61364d68ecd07eea663834f47316349274d
Orlando Groppo <orlando.groppo@gmail.com>
2020-06-30 10:56:15 +0100

    smoother icon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,26 +1,63 @@
 import React from "react";
 
-export default function Logo() {
+export default function Logo({ height = "1em", width = "1em" }) {
+  const circleRadius = 8;
+  const arcRadius = 2 * circleRadius;
+  const svgWidth = 100;
+  const svgHeight = 100;
+  const topY = 2 * circleRadius;
+  const middleY = svgHeight / 2;
+  const bottomY = svgHeight - topY;
+  const pathRightX = svgWidth - 3 * circleRadius;
+  const pathArcRightX = 7 * circleRadius;
+  const pathArcLeftX = pathArcRightX - arcRadius;
+  const pathArcTopY = topY + arcRadius;
+  const pathArcBottomY = bottomY - arcRadius;
   return (
-    <svg width="1em" height="1em" viewBox="0 0 100 100">
-      <circle fill="currentColor" cx="8" cy="50" r="8" />
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+      //style={{ background: "lightgray" }}
+    >
+      <circle fill="currentColor" cx={circleRadius} cy="50" r={circleRadius} />
       <path
         stroke="currentColor"
-        stroke-linecap="butt"
-        stroke-width="16"
+        strokeLinecap="butt"
+        strokeWidth={2 * circleRadius}
         fill="none"
-        d="M80,8 H40 A16 16 0 0 0 32 16 V76 A16 16 0 0 0 40 92 H80"
+        d={`M${pathRightX},${topY}
+        H${pathArcRightX}
+        A${arcRadius} ${arcRadius} 0 0 0 ${pathArcLeftX} ${pathArcTopY}
+        V${pathArcBottomY}
+        A${arcRadius} ${arcRadius} 0 0 0 ${pathArcRightX} ${bottomY}
+        H${pathRightX}`}
       />
       <path
         stroke="currentColor"
-        stroke-linecap="butt"
-        stroke-width="16"
+        strokeLinecap="butt"
+        strokeWidth={2 * circleRadius}
         fill="none"
-        d="M20,50 H80"
+        d={`M${3 * circleRadius},${middleY} H${pathRightX}`}
       />
-      <circle fill="currentColor" cx="92" cy="8" r="8" />
-      <circle fill="currentColor" cx="92" cy="50" r="8" />
-      <circle fill="currentColor" cx="92" cy="92" r="8" />
+      <circle
+        fill="currentColor"
+        cx={svgWidth - circleRadius}
+        cy={topY}
+        r={circleRadius}
+      />
+      <circle
+        fill="currentColor"
+        cx={svgWidth - circleRadius}
+        cy={middleY}
+        r={circleRadius}
+      />
+      <circle
+        fill="currentColor"
+        cx={svgWidth - circleRadius}
+        cy={bottomY}
+        r={circleRadius}
+      />
     </svg>
   );
 }
