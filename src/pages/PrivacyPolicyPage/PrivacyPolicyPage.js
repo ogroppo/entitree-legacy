e0fcb1d7afa1887105dc4ef84500efa4bceac9e7
Orlando Groppo <orlando.groppo@gmail.com>
2020-07-01 11:43:44 +0100

    Container fixes | ga

--- src/pages/PrivacyPolicyPage/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage/PrivacyPolicyPage.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga";
 
 export default function PrivacyPolicyPage() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, []);
 
   return (
     <Container className="pt-3">
