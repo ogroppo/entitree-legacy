a6ffa5f15162a9b813b9a9766e9c9c4e133b25d3
Orlando Groppo <orlando.groppo@gmail.com>
2020-07-08 13:18:50 +0100

    share buttons

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -9,6 +9,8 @@ import React, {
   useState,
   memo,
 } from "react";
+import ReactGA from "react-ga";
+
 import { TransformComponent } from "react-zoom-pan-pinch";
 import getItems from "../../wikidata/getItems";
 import { hierarchy } from "d3-hierarchy";
@@ -22,8 +24,16 @@ import Node from "../Node/Node";
 import Rel from "../Rel/Rel";
 import { CHILD_ID } from "../../constants/properties";
 import graphReducer, { initialState } from "./graphReducer";
-import { Button, OverlayTrigger, Tooltip, Spinner } from "react-bootstrap";
+import {
+  Button,
+  OverlayTrigger,
+  Tooltip,
+  Overlay,
+  Popover,
+  Modal,
+} from "react-bootstrap";
 import { FiMinus, FiPlus, FiPrinter } from "react-icons/fi";
+import { FaRegShareSquare } from "react-icons/fa";
 import { IoMdExpand } from "react-icons/io";
 import { RiFocus3Line } from "react-icons/ri";
 import getNodeUniqueId from "../../lib/getNodeUniqueId";
@@ -31,6 +41,24 @@ import filterSpouses from "../../lib/filterSpouses";
 import addEntityConnectors from "../../lib/addEntityConnectors";
 import getUpMap from "../../wikidata/getUpMap";
 import setPageTitle from "../../lib/setPageTitle";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  WhatsappShareButton,
+} from "react-share";
+import {
+  EmailIcon,
+  FacebookIcon,
+  RedditIcon,
+  PinterestIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
 
 export default function GraphWrapper() {
   const { showGenderColor } = useContext(AppContext);
@@ -555,49 +583,13 @@ const Graph = memo(
             </div>
           </div>
         </TransformComponent>
-        <div className="navigation">
-          <OverlayTrigger
-            placement="right"
-            overlay={<Tooltip>Zoom in</Tooltip>}
-          >
-            <Button variant="light" onClick={zoomIn}>
-              <FiPlus />
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="right"
-            overlay={<Tooltip>Zoom out</Tooltip>}
-          >
-            <Button variant="light" onClick={zoomOut}>
-              <FiMinus />
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="right"
-            overlay={
-              <Tooltip>
-                Center tree on {focusedNode && focusedNode.data.label}
-              </Tooltip>
-            }
-          >
-            <Button variant="light" onClick={recenter} disabled={!focusedNode}>
-              <RiFocus3Line />
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="right"
-            overlay={<Tooltip>Fit tree to screen</Tooltip>}
-          >
-            <Button variant="light" onClick={fitTree}>
-              <IoMdExpand />
-            </Button>
-          </OverlayTrigger>
-          <OverlayTrigger placement="right" overlay={<Tooltip>Print</Tooltip>}>
-            <Button variant="light" onClick={window.print}>
-              <FiPrinter />
-            </Button>
-          </OverlayTrigger>
-        </div>
+        <Navigation
+          zoomIn={zoomIn}
+          zoomOut={zoomOut}
+          focusedNode={focusedNode}
+          recenter={recenter}
+          fitTree={fitTree}
+        />
       </div>
     );
   },
@@ -606,3 +598,163 @@ const Graph = memo(
     return true;
   }
 );
+
+function Navigation({ zoomIn, zoomOut, focusedNode, recenter, fitTree }) {
+  const [showShareTooltip, setShowShareTooltip] = useState(false);
+  const zoomInWrapper = (e) => {
+    ReactGA.event({
+      category: "Navigation",
+      action: "zoomIn",
+    });
+    zoomIn(e);
+  };
+  const zoomOutWrapper = (e) => {
+    ReactGA.event({
+      category: "Navigation",
+      action: "zoomOut",
+    });
+    zoomOut(e);
+  };
+  const recenterWrapper = () => {
+    ReactGA.event({
+      category: "Navigation",
+      action: "recenter",
+      label: focusedNode.data.label,
+    });
+    recenter();
+  };
+  const fitTreeWrapper = () => {
+    ReactGA.event({
+      category: "Navigation",
+      action: "fitTree",
+    });
+    fitTree();
+  };
+  return (
+    <div className="Navigation">
+      <OverlayTrigger placement="right" overlay={<Tooltip>Zoom in</Tooltip>}>
+        <Button variant="light" onClick={zoomInWrapper}>
+          <FiPlus />
+        </Button>
+      </OverlayTrigger>
+      <OverlayTrigger placement="right" overlay={<Tooltip>Zoom out</Tooltip>}>
+        <Button variant="light" onClick={zoomOutWrapper}>
+          <FiMinus />
+        </Button>
+      </OverlayTrigger>
+      <OverlayTrigger
+        placement="right"
+        overlay={
+          <Tooltip>
+            Center tree on {focusedNode && focusedNode.data.label}
+          </Tooltip>
+        }
+      >
+        <Button
+          variant="light"
+          onClick={recenterWrapper}
+          disabled={!focusedNode}
+        >
+          <RiFocus3Line />
+        </Button>
+      </OverlayTrigger>
+      <OverlayTrigger
+        placement="right"
+        overlay={<Tooltip>Fit tree to screen</Tooltip>}
+      >
+        <Button variant="light" onClick={fitTreeWrapper}>
+          <IoMdExpand />
+        </Button>
+      </OverlayTrigger>
+      <OverlayTrigger placement="right" overlay={<Tooltip>Share</Tooltip>}>
+        <Button variant="light" onClick={() => setShowShareTooltip(true)}>
+          <FaRegShareSquare />
+        </Button>
+      </OverlayTrigger>
+      {showShareTooltip && (
+        <Modal
+          show={true}
+          centered
+          onHide={() => setShowShareTooltip(false)}
+          className="ShareModal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Share this tree</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="shareButton">
+              <FacebookShareButton
+                url={window.location.href}
+                quote={focusedNode.data.description}
+                hashtag="entitree"
+              >
+                <FacebookIcon /> Share on Facebook
+              </FacebookShareButton>
+            </div>
+            <div className="shareButton">
+              <TwitterShareButton
+                url={window.location.href}
+                title={document.title}
+                hashtags={["entitree"]}
+              >
+                <TwitterIcon /> Share on Twitter
+              </TwitterShareButton>
+            </div>
+            <div
+              className="shareButton"
+              url={window.location.href}
+              media={
+                focusedNode.data.images[0]
+                  ? focusedNode.data.images[0].url
+                  : null
+              }
+              description={focusedNode.data.description}
+            >
+              <PinterestShareButton>
+                <PinterestIcon /> Share on Pinterest
+              </PinterestShareButton>
+            </div>
+            <div className="shareButton">
+              <RedditShareButton
+                url={window.location.href}
+                title={document.title}
+              >
+                <RedditIcon /> Share on Reddit
+              </RedditShareButton>
+            </div>
+            <div className="shareButton">
+              <WhatsappShareButton
+                url={window.location.href}
+                title={document.title}
+              >
+                <WhatsappIcon /> Share on Whatsapp
+              </WhatsappShareButton>
+            </div>
+            <div className="shareButton">
+              <TelegramShareButton
+                url={window.location.href}
+                title={document.title}
+              >
+                <TelegramIcon /> Share on Telegram
+              </TelegramShareButton>
+            </div>
+            <div className="shareButton">
+              <Button
+                variant="none"
+                onClick={async () => {
+                  await setShowShareTooltip(false);
+                  window.print();
+                }}
+              >
+                <FiPrinter /> Print this page
+              </Button>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => setShowShareTooltip(false)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
+  );
+}
--- src/lib/setPageTitle.js
+++ src/lib/setPageTitle.js
@@ -1,6 +1,7 @@
 export default function setPageTitle(currentEntity, currentProp) {
   let title = `Entitree`;
-  if (currentProp) title = `${currentProp.label} - ` + title;
+  if (currentProp)
+    title = `${currentProp.overrideLabel || currentProp.label} - ` + title;
   if (currentEntity) title = `${currentEntity.label} - ` + title;
 
   document.title = title;
