93592c4c21e39bf6548e705e317a30d11e01c714
Orlando Groppo <orlando.groppo@gmail.com>
2020-07-08 13:53:24 +0100

    added example taxonomy

--- src/constants/examples.js
+++ src/constants/examples.js
@@ -1,27 +1,31 @@
 export const EXAMPLES = [
   {
     name: "The Queen's family",
-    href: `?q=Q9682&p=P40&l=9`,
+    href: `?q=Q9682&p=P40`,
   },
   {
     name: "Sudono Salim's descendants",
-    href: `?q=Q712401&p=P40&l=9`,
+    href: `?q=Q712401&p=P40`,
   },
   {
     name: "Dhirendra of Nepal's family",
-    href: `?q=Q5269611&p=P40&l=9`,
+    href: `?q=Q5269611&p=P40`,
   },
   {
     name: "Jesus Christ's family",
-    href: `?q=Q302&p=P40&l=9`,
+    href: `?q=Q302&p=P40`,
   },
   {
     name: "Trump's family",
-    href: `?q=Q3752663&p=P40&l=9`,
+    href: `?q=Q3752663&p=P40`,
+  },
+  {
+    name: "Mammal taxonomy",
+    href: "?p=P171&q=Q7377",
   },
   {
     name: "Pan Pan's family",
-    href: `?q=Q15928745&p=P40&l=9`,
+    href: `?q=Q15928745&p=P40`,
   },
   {
     name: "Companies owned by Cola-Cola",
