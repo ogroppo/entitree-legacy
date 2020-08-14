5dc975b3cc80a492048bba91fc2a8ec110c03aa5
Orlando Groppo <orlando.groppo@gmail.com>
2020-08-14 14:24:53 +0100

    trick for seo

--- src/wikidata/getItem.js
+++ src/wikidata/getItem.js
@@ -4,7 +4,7 @@ import formatEntity from "../lib/formatEntity";
 import { DEFAULT_LANGS_CODES } from "../constants/langs";
 
 export default async function getItem(id, languageCode) {
-  const url = await new Promise(function (resolve, reject) {
+  let url = await new Promise(function (resolve, reject) {
     try {
       resolve(
         wdk.getEntities({
@@ -17,6 +17,10 @@ export default async function getItem(id, languageCode) {
       reject(error);
     }
   });
+
+  if (url.endsWith("origin=*")) url += "&force=1";
+
+  console.log({ url });
 
   const {
     data: { entities },
