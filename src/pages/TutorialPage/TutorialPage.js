3a0d1514f3b7effd8d19edac5eb98092804af23d
Martin Schibel <mshd94@gmail.com>
2020-10-08 18:22:47 +0200

    add screenshots #91

--- src/pages/TutorialPage/TutorialPage.js
+++ src/pages/TutorialPage/TutorialPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card,Button, CardGroup, CardColumns } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 import Header from "../../layout/Header/Header";
@@ -23,8 +23,44 @@ export default function AboutPage() {
       <Container className="pb-5">
         <h1>{SITE_TITLE} Tutorial</h1>
         <p>
-          Select a Person
-          </p>
+          On the main page you can search for any kind of Object, it may be a person, taxon, concept, literally anything.
+        </p>
+        <Card style={{ width: '100%' }}>
+          <Card.Img variant="top" src="/examples/screenshot_queen.png" />
+          <Card.Body>
+            <Card.Title>Queen's family</Card.Title>
+            <Card.Text>
+              Parent's at the top, children below.
+            </Card.Text>
+            <a className="btn btn-primary" href={'/en/family_tree/Elizabeth_II'}>Open this tree</a>
+          </Card.Body>
+        </Card>
+        <CardColumns>
+
+        <Card>
+          <Card.Img variant="top" src="/examples/screenshot_bretzel.png" />
+          <Card.Body>
+            <Card.Title>Types of bread rolls</Card.Title>
+            <Card.Text>
+
+            </Card.Text>
+            <a className="btn btn-primary" href={'/en/subclass_of/Lye_roll'}>Open this tree</a>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '200%' }}>
+          <Card.Img variant="top" src="/examples/screenshot_named_after_coca.png" />
+          <Card.Body>
+            <Card.Title>Coca cola's etymology</Card.Title>
+            <Card.Text>
+              On top, what is named after coca cola, below: What was coca-cola named after.
+            </Card.Text>
+            <a className="btn btn-primary" href={'/en/named_after/Coca-Cola'}>Open this tree</a>
+          </Card.Body>
+        </Card>
+        </CardColumns>
+
+
+
       </Container>
     </div>
   );
