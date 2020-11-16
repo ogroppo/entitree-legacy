77da0c70292376722689cb3b04bbb6ff08633435
Martin Schibel <mshd94@gmail.com>
2020-11-15 22:41:07 -0600

    animal gender sorting #99

--- src/lib/sortEntities.js
+++ src/lib/sortEntities.js
@@ -1,6 +1,6 @@
 import { BIRTH_DATE_ID, GENDER_ID } from "../constants/properties";
 import getDateNumber from "./getDateNumber";
-import { HUMAN_MALE_ID } from "../constants/entities";
+import { ANIMAL_MALE_ID, HUMAN_MALE_ID } from "../constants/entities";
 export function sortByBirthDate(entities) {
   entities.sort((a, b) => {
     try {
@@ -20,7 +20,8 @@ export function sortByGender(entities) {
   entities.sort((a, b) => {
     try {
       return a.claims[GENDER_ID][0].mainsnak.datavalue.value.id ===
-        HUMAN_MALE_ID
+        HUMAN_MALE_ID ||
+        a.claims[GENDER_ID][0].mainsnak.datavalue.value.id === ANIMAL_MALE_ID
         ? -1
         : 1;
     } catch (error) {
