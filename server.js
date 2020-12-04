294ec1c4be405ad96ae8ac0ab4f7c0a1334f163a
Orlando Groppo <orlando.groppo@gmail.com>
2020-12-04 19:23:00 +0000

    Server redirect Fix #185

--- server.js
+++ server.js
@@ -15,6 +15,39 @@ const capitalise = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+const isSecure = (req) => {
+  // Check the trivial case first.
+  if (req.secure) {
+    return true;
+  }
+  // Check if we are behind Application Request Routing (ARR).
+  // This is typical for Azure.
+  if (req.headers["x-arr-log-id"]) {
+    return typeof req.headers["x-arr-ssl"] === "string";
+  }
+  // Check for forwarded protocol header.
+  // This is typical for AWS.
+  return req.headers["x-forwarded-proto"] === "https";
+};
+
+app.use((req, res, next) => {
+  console.log(req.headers.host, req.hostname);
+  if (req.hostname === "localhost") {
+    return next();
+  }
+  if (isSecure(req)) {
+    return next();
+  }
+  if (process.env.NODE_ENV === "production")
+    if (req.hostname === "entitree.com")
+      return res.redirect(301, "https://www.entitree.com");
+
+  // Note that we do not keep the port as we are using req.hostname
+  // and not req.headers.host. The port number does not really have
+  // a meaning in most cloud deployments since they port forward.
+  res.redirect("https://" + req.hostname + req.originalUrl);
+});
+
 app.get("/", function (request, response) {
   fs.readFile(indexFilePath, "utf8", function (err, template) {
     if (err) {
