d4b0dfc170937d2dd9655264552843ba16a22658
Martin Schibel <mshd94@gmail.com>
2020-12-14 15:43:40 +0700

    change birth date function

--- src/lib/sortEntities.js
+++ src/lib/sortEntities.js
@@ -1,16 +1,22 @@
 import { BIRTH_DATE_ID, GENDER_ID } from "../constants/properties";
 import getDateNumber from "./getDateNumber";
 import { ANIMAL_MALE_ID, HUMAN_MALE_ID } from "../constants/entities";
+
+/*
+Sort entities by birth date, youngest child will be on the left, if birth date is unknown they will be on the left side
+ */
 export function sortByBirthDate(entities) {
   entities.sort((a, b) => {
-    try {
+    if (a.claims[BIRTH_DATE_ID] && b.claims[BIRTH_DATE_ID]) {
       return getDateNumber(
         a.claims[BIRTH_DATE_ID][0].mainsnak.datavalue.value.time
       ) >
         getDateNumber(b.claims[BIRTH_DATE_ID][0].mainsnak.datavalue.value.time)
         ? 1
         : -1;
-    } catch (error) {
+    } else if (a.claims[BIRTH_DATE_ID]) {
+      return 1;
+    } else {
       return -1;
     }
   });
