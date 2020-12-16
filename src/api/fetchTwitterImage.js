06ada2bebe79e0bfcf428f08fb91aeeccfd85956
Martin Schibel <mshd94@gmail.com>
2020-12-16 14:07:21 +0700

    fetching Twitter image locally  #29

---
+++ src/api/fetchTwitterImage.js
@@ -0,0 +1,21 @@
+const cheerio = require("cheerio");
+const request = require("request");
+
+const cache = {};
+
+exports.fetchTwitterImage = (username, size) => {
+  const url = "https://mobile.twitter.com/" + username;
+  return new Promise((resolve) => {
+    if (cache[username]) resolve(cache[username]);
+    else
+      request(url, (_, __, body) => {
+        const $ = cheerio.load(body);
+        const url = ($(".avatar img").attr("src") || "").replace(
+          "_normal",
+          size
+        );
+        cache[username] = url;
+        resolve(url);
+      });
+  });
+};
