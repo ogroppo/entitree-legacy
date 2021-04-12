41539d2309104716e0bcc34ad706881a1f8fcbaf
Martin Schibel <mshd94@gmail.com>
2021-04-12 14:00:12 +0800

    use jsonp instead of axios , fixes #219

--- src/geni/getGeniImageUrl.js
+++ src/geni/getGeniImageUrl.js
@@ -1,11 +1,11 @@
 import getData from "../axios/getData";
+import jsonp from "jsonp-promise";
 
 export default async function getGeniImageUrl(geniId) {
   try {
-    const data = await getData(
-      `https://cors-anywhere.herokuapp.com/https://www.geni.com/api/profile-g${geniId}`,
-      { "X-Requested-With": "XMLHttpRequest" }
-    );
+    const data = await jsonp(`https://www.geni.com/api/profile-g${geniId}`, {
+      param: "callback",
+    }).promise;
     if (data && data.mugshot_urls && data.mugshot_urls.thumb) {
       return data.mugshot_urls.thumb;
     }
