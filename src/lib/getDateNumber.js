a0c596678d299dd67ef82376538ea629425bea53
Martin Schibel <mshd94@gmail.com>
2021-04-18 13:11:02 +0800

    hotfix simple claims #231

--- src/lib/getDateNumber.js
+++ src/lib/getDateNumber.js
@@ -1,8 +1,8 @@
 //Only be used for sorting! not a real number
 export default function getDateNumber(date) {
-  let year = date.substr(0, 5);
-  let month = date.substr(6, 2);
-  let day = date.substr(9, 2);
+  let year = date.substr(0, 4);
+  let month = date.substr(5, 2);
+  let day = date.substr(8, 2);
   let numericDate = Number(year + "0000") + Number(month + "00") + Number(day);
   return numericDate;
 }
